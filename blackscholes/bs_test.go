5f683524c3b0dda96b1b4ed21b6251c623758987
AgentR <AgentR@digifinex.com>
2021-05-27 15:23:05 +0800

    【library】benchmark

---
+++ blackscholes/bs_test.go
@@ -0,0 +1,18 @@
+package blackscholes
+
+import "testing"
+
+/**
+ * goos: windows
+ * goarch: amd64
+ * pkg: github.com/charlerive/library/blackscholes
+ * BenchmarkRpc
+ * BenchmarkRpc-12           261565              4335 ns/op
+ * PASS
+ */
+func BenchmarkRpc(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = NewBS("p", 4600, 5000, 0.1644, 0.025, 996.27, 0.01, 3, 0.3)
+	}
+}
